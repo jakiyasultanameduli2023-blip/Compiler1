51c867aae867fd29b1b31a6b08e918478da62340
jakiyasultanameduli2023-blip <jakiya.sultana.meduli.2023@gmail.com>
2026-05-19 20:41:54 +0600

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,87 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    string s;
+    cout <<"Enter string:";
+    cin >> s;
+
+    char == 'A';
+
+    for (int i = 0; i < s.length(); i++)
+    {
+        if(char == 'A')
+        {
+            if(s[i] == 'a')
+                char = 'B';
+            else char = 'C';
+        }
+
+        else if (char == 'B')
+        {
+            if(s[i] == 'a')
+                char = 'D';
+            else char = 'E';
+        }
+
+        else if (char == 'C')
+        {
+            if(s[i] == 'a')
+                char = 'F';
+            else char = 'G';
+        }
+
+         else if (char == 'D')
+         {
+           char == 'D';
+         }
+
+         else if (char == 'D')
+         {
+           char == 'H';
+         }
+
+        else if (char == 'E')
+        {
+            if(s[i] == 'a')
+                char == 'I'
+                else char == 'H'
+        }
+
+        else if (char == 'F')
+        {
+            if(s[i] == 'a')
+            char == 'I'
+            else char == 'J'
+        }
+        else if (char =='F')
+        {
+            char == 'K'
+        }
+        else if (char == 'G')
+        {
+            if(s[i] == 'a')
+            char == 'I'
+            else char == 'J'
+        }
+
+        else if(char =='H')
+        {
+            char == 'K'
+        }
+        else if (char == 'I')
+        {
+            char == 'K'
+        }
+
+
+    if (char == 'K')
+        cout << "Accepted";
+    else
+        cout << "Rejected";
+
+    return 0;
+
+}
